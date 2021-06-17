be2994c6f713b5e85a5bdfc8ea93c1bd4b72d980
JirenJerry <riverderio@gmail.com>
2021-06-16 21:38:57 -0600

    Fix

--- assets/scripts/locations.js
+++ assets/scripts/locations.js
@@ -1,7 +1,23 @@
 const getLocationData = async() => {
-    fetch('https://ubicaciones.paginasweb.cr/provincias.json')
+    await fetch('https://ubicaciones.paginasweb.cr/provincias.json')
         .then(response => response.json())
         .then(data => console.log(data));
+    return data;
+}
+
+const loadData = (selectControl, data) => {
+    let firstOption = document.createElement("option");
+    firstOption.selected = true;
+    firstOption.innerText = "Provincia";
+    selectControl.append(firstOption);
+    for (let index = 0; index < data.length; index++) {
+        let option = document.createElement("option");
+        option.value = index + 1;
+        option.innerText = data[index + 1];
+        selectControl.append(option);
+    }
+    return selectControl;
+
 }
 
 getLocationData();
