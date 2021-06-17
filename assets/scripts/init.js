4db9d4fba3d50c603c3c1983060c10aaa1ab9869
JirenJerry <riverderio@gmail.com>
2021-06-16 20:58:07 -0600

    Config firebase,css

---
+++ assets/scripts/init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDbiN_dTk4yRZokbPk2mnTav2DZi8hylck",
+    authDomain: "tcu-374-486.firebaseapp.com",
+    projectId: "tcu-374-486",
+    storageBucket: "tcu-374-486.appspot.com",
+    messagingSenderId: "1025527060356",
+    appId: "1:1025527060356:web:5783a0c53d9265842befd1"
+};
+
+firebase.initializeApp(firebaseConfig);
