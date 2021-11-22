12b9ad2a36e6e7b51b41ba5273d941fd0f951688
MariaJesus <maria.rodriguezbenavides@ucrso.info>
2021-11-21 19:07:57 -0600

    bb

--- src/javascript/classes/LocationLogic.js
+++ src/javascript/classes/LocationLogic.js
@@ -13,16 +13,17 @@ export class LocationLogic {
     locations.loadData(selectDistritos, data);
 
     selectProvincias.addEventListener("change", async (event) => {
-      provincia = event.target.value;
+      this.provincia = event.target.value;
 
-      let data = await locations.getCantones(provincia);
+      let data = await locations.getCantones(this.provincia);
       locations.loadData(selectCantones, data);
+      console.log( locations.loadData(selectCantones, data))
     });
 
     selectCantones.addEventListener("change", async (event) => {
       let canton = event.target.value;
 
-      let data = await locations.getDistritos(provincia, canton);
+      let data = await locations.getDistritos(this.provincia, canton);
 
       locations.loadData(selectDistritos, data);
     });
